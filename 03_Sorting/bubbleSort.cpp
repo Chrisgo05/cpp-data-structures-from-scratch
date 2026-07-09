1cb60b823ba2a17d9fc3a3723605876296cec7bd
Chrisgo05 <183979314+Chrisgo05@users.noreply.github.com>
2026-07-09 22:54:43 +0000

    implemetncion del bubble sort optimizado

---
+++ 03_Sorting/bubbleSort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void bubbleSort(int arr[], int n){
+    for (int i = 0; i < n - 1; i++){
+        bool cambio = false;
+
+        for (int j = 0; j < n - i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                cambio = true;
+            }
+        }
+
+        if(!cambio){
+            break;
+        }
+    }
+}
